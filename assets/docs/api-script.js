319bf5f75c4e3b9b444de11932d84f92aca018f9
unknown <lisaebner@gmail.com>
2019-09-06 22:32:46 -0700

    updated ajax call

---
+++ assets/docs/api-script.js
@@ -0,0 +1,27 @@
+function getRecipeiesByIngridients(ingredients) {
+  $.ajax({
+    url:
+      "https://api.spoonacular.com/recipes/findByIngredients?ingredients=bacon,+flour,+sugar&number=1&apiKey=dec60811916447f8af6fe8c1f9010bfd"
+  }).then(response => {
+    console.log("response is: ", response);
+  });
+}
+        // ---------------------------------------------------------------------------
+
+  });
+  var ingredient1 = result1[1].name;
+  var ingredient2 = result1[2].name;
+  var ingredient3 = result1[3].name;
+  
+  optString() 
+  
+  **text** | **String** | The text in which food items such as dish names and ingredients should be detected in. | 
+});
+   // 
+    {};
+    console.log(ingredient1)console.log("ingredient1 " + Endpoint + ingredient1Number + " and compare the 'name' to our 'result1[0].name'");
+    console.log("Visit " + peopleEndpoint + ingredient2Number + " and compare the 'name' to our 'result2[0].name'");
+    console.log("ingredient 1 name: " + ingredient1);
+}) 
+    for(var i = 0; i ++< )
+    console.log(ingredient1)
